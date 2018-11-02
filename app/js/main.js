d2dc4addf9e456dc3a322531b34601959407dc68
fruity4pie <fruity4pie@gmail.com>
2018-11-02 12:29:25 +0200

    added mobmenu

--- app/js/main.js
+++ app/js/main.js
@@ -22,4 +22,40 @@ $(document).ready(function() {
     lang.find('.lang-h').toggleClass('active');
   })
 
+  //MobileMenu
+	let mobHamb = $('.mob-burg-btn');
+	let mobHambClose = $('.mob-burg__h > span');
+	let mobSub = $('.mob-burg__sub');
+	let dBody = $('body');
+
+	mobSub.on('click', function(e) {
+		if($(e.target).hasClass('mob-burg__sub')) {
+			$(this).removeClass('active');
+			dBody.removeClass('active');
+		} else {
+			e.stopImmediatePropagation();
+		}
+	})
+	mobHamb.on('click', function(){
+		$(this).parent().find('.mob-burg__sub').addClass('active').find('.mob-burg__wrap').addClass('active');
+		dBody.addClass('active');
+	})
+	mobHambClose.on('click', function() {
+		mobHamb.parent().find('.mob-burg__sub').removeClass('active');
+		dBody.removeClass('active');
+	})
+
+	let mobPhone = $('.mob-phone__open');
+	let mClose = $('.mob-close');
+	let mSub = $('.mob-phone__sub');
+	mSub.on('click', function(e) {
+		$(e.target).hasClass('mob-phone__sub') ? $(this).removeClass('active') : e.stopImmediatePropagation();
+	})
+	mobPhone.on('click', function(){
+		$(this).parent().find('.mob-phone__sub').addClass('active');
+	})
+	mClose.on('click', function(){
+		mobPhone.parent().find('.mob-phone__sub').removeClass('active');
+	})
+
 })
