0017a843af1ea5cc045cd6c6b23a654d5569a4d8
fruity4pie <fruity4pie@gmail.com>
2018-11-12 16:51:42 +0200

    added map js

--- app/js/main.js
+++ app/js/main.js
@@ -24,16 +24,16 @@ $(document).ready(function() {
     let container = $(this).find('.map-b__c');
     if(container.hasClass('active')) {
       container.removeClass('active');
-      $(this).find('.map-b__t').removeClass('active');
+      // $(this).find('.map-b__t').removeClass('active');
     } else {
       $('.map-b__c').removeClass('active');
-      $('.map-b__t').removeClass('active');
+      // $('.map-b__t').removeClass('active');
       container.addClass('active');
-      $(this).find('.map-b__t').addClass('active');
-      if($(this).parent().hasClass('map-b_m')) {
-        let h = $(this).offset().top;
-        $('body,html').animate({scrollTop:h}, 1000);
-      }
+      // $(this).find('.map-b__t').addClass('active');
+      // if($(this).parent().hasClass('map-b_m')) {
+      //   let h = $(this).offset().top;
+      //   $('body,html').animate({scrollTop:h}, 1000);
+      // }
     }
   })
 
